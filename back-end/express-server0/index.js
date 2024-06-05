547180b48e59aee5e91a2fe450d5dc653f0a8d04
BrazAc <braz0502002@gmail.com>
2024-06-05 00:09:57 -0300

    criado segundo servidor local, testando diferentes endpoints  e usando js node e express

---
+++ back-end/express-server0/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server initialized and listening on port ${port}`);
+})
+
+//O que o servidor deve fazer ao receber um request get para
+//o endpoint "/"
+app.get("/", (req, res) => {
+    res.send("Homepage!");
+})
+
+//O que o servidor deve fazer ao receber um request get para
+//o endpoint "/contact"
+app.get("/contact", (req, res) => {
+    res.send("Contact page!")
+})
+
+//O que o servidor deve fazer ao receber um request get para
+//o endpoint "/about"
+app.get("/about", (req, res) => {
+    res.send("About page!")
+})
